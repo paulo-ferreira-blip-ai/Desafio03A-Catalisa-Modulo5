005a6c2b4fe2e3767c8d7f16f2a13a477ce93e42
pauloren7 <paulorenan1515@gmail.com>
2022-08-26 23:34:07 -0300

    ultima atualizacao

--- src/main/java/com/api/gerenciadordecontas/repository/ModelEntity.java
+++ src/main/java/com/api/gerenciadordecontas/repository/ModelEntity.java
@@ -2,6 +2,7 @@ package com.api.gerenciadordecontas.repository;
 
 import com.api.gerenciadordecontas.enums.StatusContas;
 import com.api.gerenciadordecontas.enums.TipoContas;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
